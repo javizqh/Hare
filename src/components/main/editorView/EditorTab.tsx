6b7482368965c2c57306242469b3bbe6f02d2e57
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-12 18:50:43 +0200

    Close button and remove splash icon

--- src/components/main/editorView/EditorTab.tsx
+++ src/components/main/editorView/EditorTab.tsx
@@ -19,7 +19,16 @@ const EditorTab = ({tab, openFileInEditor, closeFileInEditor} : {tab:any, openFi
 			onClick={(e) => {if (e.currentTarget != e.target) return; handleClick(tab)}}>
 			<div className="editor-tab-entry-icon"></div>
 			<div className="editor-tab-entry-label">{tab.name}</div>
-			<div className="editor-tab-entry-close" onClick={(e:any) => {e.stopPropagation(); handleClose(tab);}}>x</div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="#ffffff"
+				className={(tab.current) ? "editor-tab-entry-close" : "editor-tab-entry-close editor-tab-entry-close-hidden"}
+				viewBox="0 0 16 16"
+				onClick={(e:any) => {e.stopPropagation(); handleClose(tab);}}>
+				<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+			</svg>
 		</div>
 	);
 };
